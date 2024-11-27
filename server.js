b35c3c822e6e4abc613bf6fe08dae2fc670fca84
Ervin Hodzic <hodzicervin462001@gmail.com>
2024-11-27 19:13:21 +0100

    port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Define allowed origins (e.g., http://localhost:3000 for React frontend)
-const allowedOrigins = ['http://localhost:3000', 'https://tiptop-front.vercel.app'];
+const allowedOrigins = ['http://localhost:3000', 'https://tiptop-front.vercel.app', 'http://localhost:5000'];
 
 // CORS configuration
 const corsOptions = {
@@ -40,4 +40,4 @@ app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/candidate', interviewRoutes);
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
