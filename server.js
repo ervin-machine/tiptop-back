cd0997e7a0465a775f6f104a24822cdfd53c2ecf
Ervin Hodzic <hodzicervin462001@gmail.com>
2024-11-27 21:48:19 +0100

    update cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Define allowed origins (e.g., http://localhost:3000 for React frontend)
-const allowedOrigins = ['http://localhost:3000', 'https://tiptop-front.vercel.app'];
+const allowedOrigins = ['http://localhost:3000', 'https://tiptop-front.vercel.app', "https://tiptop-front-dnde0pfl6-ervin-hodzics-projects.vercel.app/login"];
 
 // CORS configuration
 const corsOptions = {
