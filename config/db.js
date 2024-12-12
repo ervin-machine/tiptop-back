660a61f15ef9a77ccff178da30dba5ff475c2466
Ervin Hodzic <hodzicervin462001@gmail.com>
2024-12-12 17:06:37 +0100

    Update env variable

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ let gfsBucket;
 
 const connectDB = async () => {
     try {
-        const connection = await mongoose.connect(process.env.MONGO_URI, {
+        const connection = await mongoose.connect(process.env.AZURE_COSMOS_CONNECTIONSTRING || process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
